d621b4b7086fb3a80ce74a278c4741b7fbfb4da1
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-10-30 15:56:16 -0300

    .

--- tdd_expressao/tests/test_validador_expressao.py
+++ tdd_expressao/tests/test_validador_expressao.py
@@ -1,7 +1,7 @@
 import pytest
 from src.validador_expressao import eh_expressao_valida
 
-def test_deve_aceitar_expressao_simples_valida(self):
+def test_deve_aceitar_expressao_simples_valida():
     assert eh_expressao_valida("1")
     assert eh_expressao_valida("1025")
     assert eh_expressao_valida("10000000")
