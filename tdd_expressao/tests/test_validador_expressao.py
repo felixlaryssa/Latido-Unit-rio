3706e4843868fd857f0b25eb0db52a6e0e323600
LUCAS ANDRADE ZANETTI <landradezanetti@gmail.com>
2025-10-30 15:42:37 -0300

    fix: bug test

--- tdd_expressao/tests/test_validador_expressao.py
+++ tdd_expressao/tests/test_validador_expressao.py
@@ -2,7 +2,7 @@ import pytest
 from src.validador_expressao import ehExpressaoValida
 
 def deveAceitarExpressaoSimplesValida(self):
-    assert ehExpressaoValida("1 + 1")
-    assert ehExpressaoValida("10 + 25")
-    assert ehExpressaoValida("1*2+3/4-5")
+    assert ehExpressaoValida("1")
+    assert ehExpressaoValida("1025")
+    assert ehExpressaoValida("10000000")
 
