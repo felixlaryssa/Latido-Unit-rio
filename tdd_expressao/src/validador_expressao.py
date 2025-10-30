e6fe6037b734c033d9e7edbeac879bbe4740f394
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-10-30 15:39:50 -0300

    .

--- tdd_expressao/src/validador_expressao.py
+++ tdd_expressao/src/validador_expressao.py
@@ -1,13 +1,14 @@
-def obter_expressao() -> None:
+def obter_expressao() -> str:
     return input()
 
-def eh_expressao_valida(): #tem que retornar bool
-    expressao = obter_expressao()
-    
+def eh_expressao_valida(expressao: str): #tem que retornar bool
+    pass
 
 def main() -> None:
     OPERADORES = '+', '-', '*', '/'
     PARENTESES = '(', ')'
+    expressao = obter_expressao()
+    
 
 
 if __name__ == '__main__':
