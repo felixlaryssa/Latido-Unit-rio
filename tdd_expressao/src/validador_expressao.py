fa93b8194caab12bbac48707c6d7bbaf9f7a44d1
LUCAS ANDRADE ZANETTI <landradezanetti@gmail.com>
2025-10-30 15:55:46 -0300

    feat: function for test1

--- tdd_expressao/src/validador_expressao.py
+++ tdd_expressao/src/validador_expressao.py
@@ -1,14 +1,28 @@
 def obter_expressao() -> str:
     return input()
 
-def eh_expressao_valida(expressao: str): #tem que retornar bool
-    pass
+def eh_expressao_valida(expressao: str):
+    
+    if expressao.isdigit():
+        return True
+    return False
+
+
+def ehExpressaoValida(expressao: str) -> bool:
+    return eh_expressao_valida(expressao)
+
 
 def main() -> None:
-    OPERADORES = '+', '-', '*', '/'
-    PARENTESES = '(', ')'
-    expressao = obter_expressao()
-    eh_expressao_valida(expressao) 
+    while True:
+        expressao = obter_expressao()
+        
+        if expressao.lower() == 'sair':
+            break
+            
+        if eh_expressao_valida(expressao):
+            print("Resultado: VÁLIDA\n")
+        else:
+            print("Resultado: INVÁLIDA\n") 
 
 
 if __name__ == '__main__':
--- tdd_expressao/tests/test_validador_expressao.py
+++ tdd_expressao/tests/test_validador_expressao.py
@@ -1,7 +1,8 @@
-import pytest
 from src.validador_expressao import ehExpressaoValida
 
-def deveAceitarExpressaoSimplesValida(self):
+
+def test_deve_aceitar_expressao_simples_valida():
+    """Happy-path: simple numeric expressions should be valid."""
     assert ehExpressaoValida("1")
     assert ehExpressaoValida("1025")
     assert ehExpressaoValida("10000000")
